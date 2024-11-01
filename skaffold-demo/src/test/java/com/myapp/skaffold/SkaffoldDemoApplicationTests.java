a584f460a299abb603dc870a54d33ec7ca2abb57
binbawany <b2bawany@gmail.com>
2024-11-01 12:40:12 +0500

    initially pushed

---
+++ skaffold-demo/src/test/java/com/myapp/skaffold/SkaffoldDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.skaffold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkaffoldDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
